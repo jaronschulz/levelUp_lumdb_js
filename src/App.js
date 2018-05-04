c891fa7881069503351b1b5e309e5f8fbf05ba56
jaronschulz <jaronschulz@gmail.com>
2018-05-04 14:20:19 +0200

    playing around with STATE, INLINE CONDITIONALS & BASIC EVENTS

--- src/App.js
+++ src/App.js
@@ -3,16 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  state = {
+    toggle: true
+  }
+  
+  toggle = () => {
+    this.setState({
+      toggle: !this.state.toggle
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Welcome text="Welcome two using Props"/>
+          <Welcome text="Welcome two using State"/>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {this.state.toggle === true && 
+          <p>This should show and hide</p>
+        }
+        <button onClick={this.toggle} >show / hide</button>
       </div>
     );
   }
@@ -20,7 +35,8 @@ class App extends Component {
 
 class Welcome extends Component {
   render() {
-    const { text } = this.props;
+    const { text, toggle } = this.props;
+    console.log(toggle);
     return(
       <h1 className="App-title">{text}</h1>
     )
