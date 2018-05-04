2bf1d88467f742244d6ca9ef91442e6563d42d14
jaronschulz <jaronschulz@gmail.com>
2018-05-04 12:12:17 +0200

    Playing around with Props

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const welcome = 'Welcome 2 React';
-
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Welcome />
+          <Welcome text="Welcome two using Props"/>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -22,8 +20,9 @@ class App extends Component {
 
 class Welcome extends Component {
   render() {
+    const { text } = this.props;
     return(
-      <h1 className="App-title">{welcome}</h1>
+      <h1 className="App-title">{text}</h1>
     )
   }
 }
