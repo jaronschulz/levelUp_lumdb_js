a0c95c1cff21bfd3aa4ed87b2c9243dc78d25d42
jaronschulz <jaronschulz@gmail.com>
2018-05-04 11:53:23 +0200

    playing around with App.js and components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const welcome = 'Welcome 2 React';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <Welcome />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -18,4 +20,13 @@ class App extends Component {
   }
 }
 
+class Welcome extends Component {
+  render() {
+    return(
+      <h1 className="App-title">{welcome}</h1>
+    )
+  }
+}
+
 export default App;
+
